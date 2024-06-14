c28f94172f57bd90d96a044166edf2b9cd573979
MaulaNaja4 <maulanaja4@gmail.com>
2024-06-14 19:00:21 +0700

    add model

--- controllers/predictionController.js
+++ controllers/predictionController.js
@@ -1,17 +1,43 @@
 const { Prediction, User } = require("../models");
+const tf = require('@tensorflow/tfjs-node');
+const fetch = require('node-fetch');
+
+let model;
+
+const loadModel = async () => {
+  try {
+    model = await tf.loadLayersModel('https://storage.googleapis.com/c241-ps499-fraud-guard-storage/tfjs_model/model.json');
+    console.log('Model loaded successfully');
+  } catch (error) {
+    console.error('Error loading model:', error);
+  }
+};
+
+loadModel();
 
 const checkSpam = async (req, res) => {
   try {
     const { message, label } = req.body;
-    const prediction = await Prediction.create({
+
+    const processedMessage = preprocessMessage(message);
+    
+    const predictionTensor = model.predict(processedMessage);
+    const prediction = predictionTensor.dataSync()[0];
+
+    const newPrediction = await Prediction.create({
       message,
       label,
       UserId: req.user.id,
+      prediction,
     });
-    res.status(201).send(prediction);
+    res.status(201).send(newPrediction);
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
+};
+
+const preprocessMessage = (message) => {
+  return tf.tensor([message.split('').map(char => char.charCodeAt(0))]);
 };
 
 const getPredictionHistory = async (req, res) => {
@@ -20,7 +46,6 @@ const getPredictionHistory = async (req, res) => {
       where: { UserId: req.user.id, deletedAt: null },
       order: [["timestamp", "DESC"]],
     });
-    // res.send(req.user.id);
     res.send(predictions);
   } catch (error) {
     res.status(500).send({ error: error.message });
